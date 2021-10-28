49d02a4e218fb0aac65924744db1b2a997a1faa8
hanyemi09 <54048632+hanyemi09@users.noreply.github.com>
2021-10-28 16:47:49 +0800

    Joystick movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,18 +4,46 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+
+    FixedJoystick joystick;
+    BoxCollider2D boxCollider;
+    Vector3 moveDelta;
     // Start is called before the first frame update
     void Start()
     {
-        
+        boxCollider = GetComponent<BoxCollider2D>();
+        joystick = FindObjectOfType<FixedJoystick>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        for(int i = 0; i < Input.touchCount; i++)
+
+        float x = joystick.Horizontal;
+        float y = joystick.Vertical;
+
+        Debug.Log(x);
+        Debug.Log(y);
+        //float x = Input.GetAxisRaw("Horizontal");
+        //float y = Input.GetAxisRaw("Vertical");
+
+        //horizontalMove = joystick.Horizontal * runSpeed;
+
+        // Reset moveDelta
+        moveDelta = new Vector3(x, y, 0);
+        
+        // Swap sprite directions
+        if(moveDelta.x > 0)
         {
-            Vector3 touchPosition = Camera.main.ScreenToWorldPoint(Input.touches[i].position);
+            transform.localScale = Vector3.one;
         }
+        else if(moveDelta.x < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        // Move
+        transform.Translate(moveDelta * Time.deltaTime);
     }
 }
