1bdcc6858fdeb2778ad25b93d59226a26bf13724
hanyemi09 <54048632+hanyemi09@users.noreply.github.com>
2021-11-24 13:44:21 +0800

    Photon
    - Added join room as text

--- Assets/Scripts/Photon/CreateAndJoinRooms.cs
+++ Assets/Scripts/Photon/CreateAndJoinRooms.cs
@@ -1,20 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Photon.Pun;
 using UnityEngine.SceneManagement;
 
 public class CreateAndJoinRooms : MonoBehaviourPunCallbacks
 {
+    public InputField createRoomText;
+    public InputField joinRoomText;
 
     public void CreateRoom()
     {
-        PhotonNetwork.CreateRoom("yeap");
+        PhotonNetwork.CreateRoom(createRoomText.text);
     }
 
     public void JoinRoom()
     {
-        PhotonNetwork.JoinRoom("yeap");
+        PhotonNetwork.JoinRoom(joinRoomText.text);
 
     }
     public override void OnJoinedRoom()
